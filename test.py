694a42f2e868bfd96c100a757325df6efc501d6d
Immanuel Allotey <imma.adt@gmail.com>
2025-08-13 00:42:59 +0000

    Update test.py

--- test.py
+++ test.py
@@ -452,9 +452,9 @@ def run_interactive_tests():
 if __name__ == "__main__":
     import sys
     
-    if len(sys.argv) > 1 and sys.argv[1] == "--interactive":
-        run_interactive_tests()
-    else:
-        run_all_tests()
+    # if len(sys.argv) > 1 and sys.argv[1] == "--interactive":
+    run_interactive_tests()
+    # else:
+    #     run_all_tests()
 
         
